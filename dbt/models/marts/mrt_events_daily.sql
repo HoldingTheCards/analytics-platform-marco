e0f1d6bb8e991ed50bbbb9077e3bc2f7a26417f3
Marco Knieling <Knielin4@students.uni-marburg.de>
2025-10-11 19:08:45 +0200

    feat: local duckdb setup + env

--- dbt/models/marts/mrt_events_daily.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
--- Simple mart: daily event counts
-select
-  date_trunc('day', event_ts) as day,
-  event_name,
-  count(*) as events
-from {{ ref('fct_events') }}
-group by 1,2
-order by 1,2
--- dbt/models/staging/stg_events.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='view') }}
-select
-  cast(event_id as text) as event_id,
-  cast(user_id as text) as user_id,
-  cast(session_id as text) as session_id,
-  cast(event_name as text) as event_name,
-  cast(event_ts as timestamp) as event_ts,
-  cast(consent as boolean) as consent
-from read_csv_auto('../data/events.csv')
